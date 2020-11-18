b5b78c8995f98eec07a6112eab52e64e5b1a552f
Alejandro  Mauricio <abecerraguz@MacBook-Pro-de-Alejandro.local>
2020-11-18 15:22:57 -0300

    Se agrega el segundo apellido

--- layout.js
+++ layout.js
@@ -3,7 +3,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Alejandro Becerra Guzmán'
+const name = 'Alejandro Mauricio Becerra Guzmán'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
