57950c53c886b27b464f3ac4ac926936b3cbe483
Alejandro  Mauricio <abecerraguz@MacBook-Pro-de-Alejandro.local>
2020-11-18 14:59:15 -0300

    Se cambia el apellido

--- layout.js
+++ layout.js
@@ -3,7 +3,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Alejandro Becerraaaaaa G.'
+const name = 'Alejandro Becerra Guzmán'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
